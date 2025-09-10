af193c0e7cd563a5f51932103545cba84b761685
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-10 22:18:28 +0500

    masala

---
+++ main.js
@@ -0,0 +1,44 @@
+//1-masala
+for (let i = 1; i <= 140; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+
+//2-masala
+for (let i = 100; i <= 240; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+//3-masala
+for (let i = 40; i <= 180; i++) {
+  if (i % 7 === 0) {
+    console.log(i);
+  }
+}
+//4-masala
+for (let i = 50; i <= 440; i++) {
+  if (i % 11 === 0) {
+    console.log(i);
+  }
+}
+//5-masala
+for (let i = 90; i <= 210; i++) {
+  if (i % 25 === 0) {
+    console.log(i);
+  }
+}
+//6-masala
+for (let i = 25; i <= 690; i++) {
+  if (i % 5 === 0 && i % 9 === 0) {
+    console.log(i);
+  }
+}
+//7-masala
+for (let i = 1; i <= 4000; i++) {
+  if (i % 11 === 0 && i % 3 === 0 && i % 9 === 0) {
+    console.log(i);
+  }
+}
